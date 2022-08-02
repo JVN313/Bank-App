0915ed12f1249313c79f406d2f553ba88cd5103c
JVN313 <jvncovington@gmail.com>
2022-08-02 16:06:44 -0400

    updated home screen options

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
+from telnetlib import PRAGMA_HEARTBEAT
 from classes import *
 from login import *
 from create_account import *
 
 james =''
 james = New_User(james)
+
 def Home_Screen(user):
     print("")
     print("Welcome Back")
     print(user.user_name)
+    print("To Show Balance PRESS 'B'")
+    print("To Deposit PRESS 'D' ")
+    print("To Withdraw PRESS 'W' ")
+    user_desicion = input("To Transfer Money PRESS 'T'\n: ").upper()
+    if user_desicion == "B":
+        print(f"Your Account Balance Is: {user.account_bal}")
 
 def Enter_Screen():
     print("Welcome to Jamm Bank!")
@@ -30,4 +38,4 @@ def Enter_Screen():
             user_desiscion= input("Please Enter 'L' to Login or 'N' for New Useres.\n").upper()
 
 Enter_Screen()
-#TODO create __repr__ for Account Class finish, create UI
+#TODO finish creating the UI starting with the Home_Screen Function options
